4a8b082943caa6894326d1d06113ea442be1cb82
Moballighul islam <89847264+Moballigh5225@users.noreply.github.com>
2024-03-17 21:25:03 +0000

    conversion of datatypes

---
+++ 01_basics/01_variables/conversionOperation.js
@@ -0,0 +1,57 @@
+// codes
+// const fore = "33";
+// const goal = "33abc";
+// const sole = null;
+// const hole = undefined;
+// const pole = false;
+// const same = "mobi";
+
+// let nameNum = Number(same);
+
+// console.log(nameNum,typeof(nameNum));
+
+
+
+// let secondScore = Number(fore)
+// let thirdScore = Number(goal)
+// let forthScore = Number(sole)
+// let fifthScore = Number(hole)
+// let sixthScore = Number(pole)
+
+// console.log(typeof(fore));
+// console.log(typeof(secondScore));
+// console.log(typeof(thirdScore),thirdScore,);
+// console.log(typeof(forthScore),forthScore);
+// console.log(typeof(fifthScore),fifthScore);
+// console.log(typeof(sixthScore),sixthScore);
+
+
+// Notes 
+// true  = 1
+// false = 0
+// "33" => 33
+// "33abc" => number value = Nan 
+// null => number value = 0
+// undefined => number value = NaN
+// "mobi" => number value = Nan
+
+
+
+// codes
+// let isLoggedIn = true
+// let isLoggedIn = ""
+// let isLoggedIn = "mobi"
+// let logged = Boolean(isLoggedIn)
+// console.log(logged,typeof(logged));
+
+// notes
+// 1 => true
+// 0 => false
+// "" => false boolean-type
+// "mobi" => true boolean-type
+
+
+let someNumber = 33;
+let StringNumber = String(someNumber)
+
+console.log(StringNumber,typeof(StringNumber));
